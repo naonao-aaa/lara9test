55032c13e1955d5ea81d06913a4dd7f7b8532de9
naonao-aaa <naonao1814naonao@gmail.com>
2023-07-29 11:43:38 +0900

    ブログのタイトル一覧表示

--- tests/Feature/Http/Controllers/PostListControllerTest.php
+++ tests/Feature/Http/Controllers/PostListControllerTest.php
@@ -2,24 +2,24 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PostListControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     function TOPページで、ブログ一覧が表示される()
     {
-        // Ver.8.51未満の場合で、500エラーが出た場合のエラー確認方法
-        //
-        // $this->withoutExceptionHandling();
-        // ブラウザで確認できる場合は、ブラウザで確認する方法もある
-        // エラーログを確認する
-
-        // $this->withoutExceptionHandling();        
+        $post1 = Post::factory()->create(['title' => 'ブログのタイトル1']);
+        $post2 = Post::factory()->create(['title' => 'ブログのタイトル2']);
 
         $this->get('/')
-            ->assertOk();
+            ->assertOk()
+            ->assertSee('ブログのタイトル1')
+            ->assertSee('ブログのタイトル2');
     }
 }
