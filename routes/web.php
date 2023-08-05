ff1b89c24a58b6c1abedeaab601bcb35c34801be
naonao-aaa <naonao1814naonao@gmail.com>
2023-08-05 10:46:10 +0900

    ログイン画面を開ける

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,16 @@
 <?php
 
+use App\Http\Controllers\Mypage\UserLoginController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', [PostController::class, 'index']);
-
 Route::get('posts/{post}', [PostController::class, 'show'])
     ->name('posts.show')
     ->whereNumber('post');
 
-
 Route::get('signup', [SignupController::class, 'index']);
-
 Route::post('signup', [SignupController::class, 'store']);
 
+Route::get('mypage/login', [UserLoginController::class, 'index']);
