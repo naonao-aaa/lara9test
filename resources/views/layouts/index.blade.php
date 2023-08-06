a52e70dfaa49c967d75376510cdbc0db58ace0f9
naonao-aaa <naonao1814naonao@gmail.com>
2023-08-06 10:52:21 +0900

    ブラウザ表示の確認

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -8,7 +8,23 @@
 
 <body>
 
+<nav>
+    <li><a href="/">TOP（ブログ一覧）</a></li>
+
+    @auth
+        <li><a href="{{ route('mypage.posts') }}">マイブログ一覧</a></li>
+        <li>
+            <form method="post" action="{{ route('logout') }}">
+            @csrf<input type="submit" value="ログアウト">
+            </form>
+        </li>
+    @else
+        <li><a href="{{ route(('login')) }}">ログイン</a></li>
+    @endauth
+</nav>
+
 @yield('content')
 
 </body>
+
 </html>
