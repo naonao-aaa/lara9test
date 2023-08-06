1874e7067bbc65e895b3b19ccc67f5da911c637c
naonao-aaa <naonao1814naonao@gmail.com>
2023-08-06 11:55:26 +0900

    マイページ、自分のブログのみ一覧表示

--- resources/views/mypage/posts/index.blade.php
+++ resources/views/mypage/posts/index.blade.php
@@ -9,23 +9,26 @@
 <hr>
 
 
-{{-- <table>
+<table>
     <tr>
         <th>ブログ名</th>
     </tr>
     @foreach($posts as $post)
     <tr>
         <td>
+            {{ $post->title }}
+        </td>
+        {{-- <td>
             <a href="{{ route('mypage.post.edit', $post) }}">{{ $post->title }}</a>
         </td>
         <td>
             <form method="post" action="{{ route('mypage.post.delete', $post) }}">
                 @csrf @method('delete') <input type="submit" value="削除">
             </form>
-        </td>
+        </td> --}}
     </tr>
     @endforeach
-</table> --}}
+</table>
 
 
 @endsection
