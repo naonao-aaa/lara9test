297ae030c474434b7599fd857e460779ea639f22
naonao-aaa <naonao1814naonao@gmail.com>
2023-08-09 22:49:30 +0900

    aliaseについて

--- app/Actions/StrRandom.php
+++ app/Actions/StrRandom.php
@@ -2,10 +2,13 @@
 
 namespace App\Actions;
 
+use Illuminate\Support\Str;
+
 class StrRandom
 {
     public function get($length)
     {
-        return \Str::random($length);
+        // return \Str::random($length);   //aliaseを使う書き方
+        return Str::random($length);       //aliaseを使わない書き方
     }
 }
